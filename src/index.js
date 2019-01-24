e1f9ba2d702b93febe3c2c00e5f4889968772b5b
orelkabets <elkabetsor@gmail.com>
2019-01-24 17:37:59 +0200

    Update index.js
    add internal transaction watcher

--- src/index.js
+++ src/index.js
@@ -10,5 +10,8 @@ if (conf.TOKEN_CONTRACT_ADDRESSES.length > 0) {
     console.log('Started watching Token transfers\n');
 }
 
+console.log('Started watching internal transactions');
+watcher.TraceInternalContractTransactions();
+
 watcher.watchLatestBlocks();
 console.log('watching latest blocks for confirmations count\n');
